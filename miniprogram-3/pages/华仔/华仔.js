c99d42defaceba699c35406217002bfdba3d9dde
zelin <zelin@zelindeMacBook-Pro-7.local>
2020-05-19 09:17:56 +0800

    000

---
+++ miniprogram-3/pages/华仔/华仔.js
@@ -0,0 +1,88 @@
+// pages/华仔/华仔.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    name:"刘德华",
+    waiwenming:"Andy lau",
+    nickName:"华仔",
+    nationion:"中国",
+    mingzu:"汉族",
+    xingzuo:"水瓶座",
+    id:"01",
+    condtion:true,
+    isBoy:true,
+    isGirl:false,
+    a:1,
+    b:2,
+    c:3,
+    length:5,
+    arr:[
+      {name:"刘德华",title:'中文名'},
+      {name:"Andy lau",title:'外文名'},
+      {name:"华仔",title:'别名'},
+      {name:"中国",title:'国籍'},
+      {name:"汉族",title:'民族'},
+      {name:"水瓶座",title:'星座'},
+
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ miniprogram-3/pages/计程车收费/计程车收费.js
@@ -0,0 +1,67 @@
+// pages/计程车收费/计程车收费.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    mile:20,
+    str:'人生得意须尽欢，莫使金樽空对月'
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
