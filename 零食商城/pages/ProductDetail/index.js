26c403b0cb520a98ef247ddb055c8e45b1fc1cac
tanlei8888 <1097481748@qq.com>
2020-05-26 00:22:17 +0800

    5.25.12.22

--- 零食商城/pages/ProductDetail/index.js
+++ 零食商城/pages/ProductDetail/index.js
@@ -12,17 +12,10 @@ Page({
     //储存修改过后的json 传给本地储存 storage
     arr:[],
     //购物车里的数量
-    count:1
+    count:1,
+    activeTab:"detail"
   },
-  fn:function(e){
-    let productData = require("../../data/details-data")
-    let carData = this.data.id
-    //获取到通过id属性接收到的那一组json 和 购物车数量
-    this.setData({
-      productData: productData[carData],
-      count:productData[carData].i
-    })
-  },
+
   //加入购物车按钮
   joinCar:function(e){
     //每次点击先获取到原来的本地储存的数据
@@ -31,17 +24,13 @@ Page({
     let i = getData.findIndex(e=> e.id === this.data.productData.id)
     //-1 表示不存在 push进本地储存
     if(i === -1){
+      this.data.productData.i =  this.data.count
       getData.push(this.data.productData)
     }else{
-      if(getData.length !== 0){
         //已经存在该商品 只添加数量至购物车
         getData[i].i = getData[i].i + this.data.count
         getData[i].money = (getData[i].i * getData[i].price).toFixed(2)
-      }
-      console.log(getData[i].i)
     }
-    console.log(getData)
-    
     //接受本地数据
     this.setData({
       arr:getData,
@@ -61,18 +50,31 @@ Page({
     })
     //每次点击 改变 通过 id 获取到的这个商品的总价money数据，单价乘以数量 
     this.data.productData.money = (this.data.count*this.data.productData.price).toFixed(2)
-    console.log(this.data.productData.money)
   },
-
+  //切换底部详情样式事件
+  handleChange:function(e){
+    this.setData({
+      activeTab:e.target.dataset.tab
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     //通过跳转到这个页面的options得到被点击的那个商品的ID
-    this.setData({
-      id: options.id
-    })
-    this.fn()
+    let productData = require("../../data/details-data")
+    let carData = options.id
+    if(carData === undefined){
+      this.setData({
+        productData: productData.qiyiguo,
+      })
+    }else{
+      this.setData({
+        productData: productData[carData],
+        count:productData[carData].i,
+        id: carData
+      })
+    }
   },
 
   /**
